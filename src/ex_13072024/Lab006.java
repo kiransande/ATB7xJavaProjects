43f68787fe5b5dff0fcd6dd02781d8fc9072db8a
Kiran Kadam <kiran3181kk@gmail.com>
2024-07-14 19:05:32 +0530

    commit new

--- src/ex_13072024/Lab006.java
+++ src/ex_13072024/Lab006.java
@@ -11,7 +11,7 @@ public class Lab006 {
 
         System.out.println("Your age is ->" + age);
         System.out.printf("Your age is %d",age);
-
+        System.out.println("\n");
         System.out.println("My name is->"+s);
         System.out.printf("My name is-> %s",s);
 
---
+++ src/ex_13072024/Lab007.java
@@ -0,0 +1,9 @@
+package ex_13072024;
+
+public class Lab007 {
+    public static void main(String[] args) {
+        int n = 9;
+        System.out.printf("%d",n);
+
+    }
+}
