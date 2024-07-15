794a0dc4d3d5c5332bba13d63d2a97a81355101d
Kiran Kadam <kiran3181kk@gmail.com>
2024-07-15 22:22:51 +0530

    commit new program

--- src/ex_13072024/Lab006.java
+++ src/ex_13072024/Lab006.java
@@ -1,12 +1,12 @@
 package ex_13072024;
 
-import java.sql.SQLOutput;
+
 
 public class Lab006 {
     public static void main(String[] args) {
         int age= 34;
         String s= "kiran";
-        float f= 3.14f;
+        float pie = 3.14f;
         char c= 'K';
 
         System.out.println("Your age is ->" + age);
@@ -14,6 +14,8 @@ public class Lab006 {
         System.out.println("\n");
         System.out.println("My name is->"+s);
         System.out.printf("My name is-> %s",s);
+        System.out.println("\n");
+        System.out.println(c);
 
     }
 }
---
+++ src/ex_13072024/Lab008.java
@@ -0,0 +1,18 @@
+package ex_13072024;
+
+public class Lab008 {
+    public static void main(String[] args) {
+        int x= 10;
+        int y= 20;
+        System.out.println(x+y);
+
+        String s1= "Kiran";
+        String s2= "Amit";
+        System.out.println(s1+s2);
+
+        System.out.println(x+y+s1+s2);
+        System.out.println(s1+s2+x+y);
+        System.out.println(s1+x+y+s2);
+    }
+
+}
