eac7be7ac97c5c6b3ffdc8b576d1519b4103b3be
Kiran Kadam <kiran3181kk@gmail.com>
2024-07-18 22:52:03 +0530

    commit new programs

---
+++ src/ex_14072024/Lab018.java
@@ -0,0 +1,14 @@
+package ex_14072024;
+
+public class Lab018 {
+    public static void main(String[] args) {
+        // || - OR
+
+        System.out.println(true || true);
+        System.out.println(true || false);
+        System.out.println(false || true);
+        System.out.println(false || false);
+
+
+    }
+}
