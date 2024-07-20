0d59747fcb50111d64f22418fde06a340dc56cc0
Kiran Kadam <kiran3181kk@gmail.com>
2024-07-20 17:26:21 +0530

    commit new programs

---
+++ src/ex_19072024/Lab025.java
@@ -0,0 +1,10 @@
+package ex_19072024;
+
+public class Lab025 {
+    public static void main(String[] args) {
+        short a=20;
+        char c= 'A'; //ASCII value for A=65
+        int b= a+c;
+        System.out.println(b);
+    }
+}
