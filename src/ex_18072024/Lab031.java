8ead6aec0f3a8183bed05e53f8c906178bc54307
Kiran Kadam <kiran3181kk@gmail.com>
2024-07-22 21:30:12 +0530

    commit new programs 2

---
+++ src/ex_18072024/Lab031.java
@@ -0,0 +1,17 @@
+package ex_18072024;
+
+public class Lab031 {
+    public static void main(String[] args) {
+        int a= 10;
+        int result= a++;
+        System.out.println(a);
+        System.out.println(result);
+        // Exp=10; a=11;
+
+        int b= 20;
+        int result1= ++b;
+        System.out.println(b);
+        System.out.println(result1);
+        // Exp=11; b=11;
+    }
+}
