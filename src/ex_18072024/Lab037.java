39f63d13e00d7b86b91cf2e45b46cbeb0875d3a7
Kiran Kadam <kiran3181kk@gmail.com>
2024-07-22 23:16:25 +0530

    commit new programs 3

--- src/ex_18072024/Lab037.java
+++ src/ex_18072024/Lab037.java
@@ -4,6 +4,8 @@ public class Lab037 {
     public static void main(String[] args) {
         String name = "Kiran"; // Assignment Operators
         String name2 = new String("Kiran"); // New Operator
+        System.out.println(name);
+        System.out.println(name2);
 
         // Both of them, actually save the name, name2 in different ways JVM
         // How they store the values in the JVM?
@@ -16,4 +18,5 @@ public class Lab037 {
 
     }
 }
-}
+
+
