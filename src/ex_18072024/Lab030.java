db57ff5698ced94fbeef083244155e22fb7f853f
Kiran Kadam <kiran3181kk@gmail.com>
2024-07-22 21:02:56 +0530

    commit new programs 2

--- src/ex_18072024/Lab030.java
+++ src/ex_18072024/Lab030.java
@@ -8,11 +8,11 @@ public class Lab030 {
 
         // pre - increment ++operand
         // value is incremented first and then stored in the result.
-//        int a = 10;
-//        int b = ++a; // a = a+1;
-//        // Exp = 11 , a = 11
-//        System.out.println(a);
-//        System.out.println(b);
+        int a = 10;
+        int b = ++a; // a = a+1;
+        // Exp = 11 , a = 11
+        System.out.println(a);
+        System.out.println(b);
 
 
 //
