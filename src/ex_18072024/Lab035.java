e63edc404d55893b17725fd244754f5ffa3ce58e
Kiran Kadam <kiran3181kk@gmail.com>
2024-07-22 23:14:36 +0530

    commit new programs 3

---
+++ src/ex_18072024/Lab035.java
@@ -0,0 +1,16 @@
+package ex_18072024;
+
+import java.sql.SQLOutput;
+
+public class Lab035 {
+    public static void main(String[] args) {
+        int a = 87;
+        long b = 91;
+        String s1 = "KK";
+        String s2 = "VV";
+        System.out.println(s1+s2+a+b);
+        System.out.println(s1+s2+(a+b));
+        System.out.println(s1+s2+87+91);
+        System.out.println(a+b+s1+s2);
+    }
+}
---
+++ src/ex_18072024/Lab037.java
@@ -0,0 +1,19 @@
+package ex_18072024;
+
+public class Lab037 {
+    public static void main(String[] args) {
+        String name = "Kiran"; // Assignment Operators
+        String name2 = new String("Kiran"); // New Operator
+
+        // Both of them, actually save the name, name2 in different ways JVM
+        // How they store the values in the JVM?
+
+
+        // SCP nad Heap - JVM
+
+
+
+
+    }
+}
+}
