b65e91946b97b5f5826895bbb7fadbe47888f018
Kiran Kadam <kiran3181kk@gmail.com>
2024-08-04 20:46:25 +0530

    commit changes 2

---
+++ src/ex_20072024/Lab048.java
@@ -0,0 +1,28 @@
+package ex_20072024;
+
+public class Lab048 {
+    public static void main(String[] args) {
+        // Condition and Loops
+        //
+        // Condition ?
+        // age  > 18 -> you allowed to / Vote
+        //
+        //  if age > 18 -> vote, else -> not allowed.
+
+        // Condition - Java -> switch , If
+        // syntax
+//        if (condition ==> true or false){
+//
+//        }else{
+//
+//        }
+
+        int age=17;
+        if(age>18){
+            System.out.println("You are eligible to Vote");
+        }
+         else{
+            System.out.println("Not Allowed!");
+        }
+    }
+}
