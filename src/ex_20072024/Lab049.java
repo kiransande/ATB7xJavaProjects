16949bd5aa11af3642a5359d9bd590dc911dc18d
Kiran Kadam <kiran3181kk@gmail.com>
2024-08-04 22:54:27 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/ex_20072024/Lab049.java
@@ -0,0 +1,13 @@
+package ex_20072024;
+
+public class Lab049 {
+    public static void main(String[] args) {
+            boolean b = !true;
+            if(b == false){
+                System.out.println("b is true");
+            }else {
+                System.out.println("b is not true");
+            }
+
+        }
+}
---
+++ src/ex_20072024/Lab050.java
@@ -0,0 +1,13 @@
+package ex_20072024;
+
+public class Lab050 {
+    public static void main(String[] args) {
+        int a= 10;
+        if(a==5){
+            System.out.println("LOL");
+        }
+        else {
+            System.out.println("HaHa");
+        }
+    }
+}
