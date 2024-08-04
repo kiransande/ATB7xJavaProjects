c6e7e29bc8a7f9c7b7a7a041ccc17e86dbebdd39
Kiran Kadam <kiran3181kk@gmail.com>
2024-08-04 19:13:36 +0530

    commit changes 2

---
+++ src/ex_18072024/Lab046.java
@@ -0,0 +1,12 @@
+package ex_18072024;
+
+public class Lab046 {  public static void main(String[] args) {
+    // Max between two numbers
+    int a= 10;
+    int b= 20;
+    int max = Math.max(a, b);
+    int min = a < b ? a : b;
+    System.out.println(max);
+    System.out.println(min);
+}
+}
---
+++ src/ex_18072024/Lab047.java
@@ -0,0 +1,11 @@
+package ex_18072024;
+import java.lang.Math;
+
+public class Lab047 {
+    public static void main(String[] args) {
+        double sq = Math.sqrt(2);
+        int result = Math.max(10,23);
+        System.out.println(sq);
+        System.out.println(result);
+    }
+}
