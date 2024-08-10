7daf53883cfe01193078310cda6eb3f8dab40269
Kiran Kadam <kiran3181kk@gmail.com>
2024-08-10 19:52:33 +0530

    while loop example

---
+++ src/ex_21072024/Lab071.java
@@ -0,0 +1,16 @@
+package ex_21072024;
+
+public class Lab071 {
+    //loop with condition
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 10; i++) {
+            if (i == 5) {
+                System.out.println("Five");
+            } else {
+                System.out.println(i);
+
+            }
+        }
+    }
+}
