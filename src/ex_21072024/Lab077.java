16fecf75cb98eb1c371e3bc977df58ca6195e452
Kiran Kadam <kiran3181kk@gmail.com>
2024-08-20 21:54:20 +0530

    while loop example

---
+++ src/ex_21072024/Lab077.java
@@ -0,0 +1,10 @@
+package ex_21072024;
+
+public class Lab077 {
+    public static void main(String[] args) {
+        //Infinite loop
+        while (true){
+            System.out.println("Kiran");
+        }
+    }
+}
---
+++ src/ex_21072024/Lab080.java
@@ -0,0 +1,21 @@
+package ex_21072024;
+
+public class Lab080 {  public static void main(String[] args) {
+    // while, for -?
+    // do while - while - 1 change - minor change
+
+    // While -> int -> condition -> body -> increment / decrement
+
+    // Do While
+    // int -> body -> condition -> increment / decrement
+
+    int a = 0;
+    do {
+        System.out.println(a);
+        a++;
+    }while(a < 10);
+
+
+
+}
+}
